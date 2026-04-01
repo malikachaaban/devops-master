c1e626f63f982e0a12a8cc7352354d6af3e4d5d7
Salama Boussaid <salamaboussaid132@gmail.com>
2026-04-01 22:25:31 +0100

    Modification dans app

--- app/app.py
+++ app/app.py
@@ -17,7 +17,7 @@ HTML_PAGE = """<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Gestionnaire des Tâches</title>
+<title>Gestionnaire de Tâches</title>
 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,700;0,900;1,400&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 <style>
   :root {
